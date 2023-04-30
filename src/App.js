5c3e8a53b8405e9349dda0094764619aeb0c438e
Now Sweat <nowsweat@gmail.com>
2023-04-30 13:51:20 -0300

    Add responsive web and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { useMediaQuery } from "@material-ui/core";
 
 function App() {
   // Function to smoothly scroll to a specific section of the page when a navigation button is clicked
@@ -7,47 +8,89 @@ function App() {
     const section = document.getElementById(sectionID);
     section.scrollIntoView({ behavior: "smooth" });
   };
+  // Detect whether the screen is small or not
+  const isSmallScreen = useMediaQuery("(max-width: 767px)");
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Function to toggle the dropdown menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <a>
       <div className="container">
+        {/* Navigation bar that allows easy access to different sections of the site*/}
         <div className="buttons">
-          {/* Navigation bar that allows easy access to different sections of the site*/}
-          <a onClick={() => scrollToSection("contact")} className="button">
-            Contact
-          </a>
-          <a onClick={() => scrollToSection("about")} className="button2">
-            About PlantFashion
-          </a>
-          <a onClick={() => scrollToSection("inspiration")} className="button3">
-            Inspirations
-          </a>
-          <a onClick={() => scrollToSection("plantshop")} className="button4">
-            Plant Shop
-          </a>
+          {isSmallScreen ? (
+            // Show dropdown menu with 3 dots for small screens
+            <div className="dropdown">
+              <button className="dropbtn" onClick={toggleMenu}>
+                &#8942;
+              </button>
+              {isOpen && (
+                <div className="dropdown-content">
+                  <a onClick={() => scrollToSection("contact")}>Contact</a>
+                  <a onClick={() => scrollToSection("about")}>
+                    About PlantFashion
+                  </a>
+                  <a onClick={() => scrollToSection("inspiration")}>
+                    Inspirations
+                  </a>
+                  <a onClick={() => scrollToSection("plantshop")}>Plant Shop</a>
+                </div>
+              )}
+            </div>
+          ) : (
+            // Show full menu for larger screens
+            <nav>
+              <a onClick={() => scrollToSection("contact")} className="button">
+                Contact
+              </a>
+              <a onClick={() => scrollToSection("about")} className="button2">
+                About PlantFashion
+              </a>
+              <a
+                onClick={() => scrollToSection("inspiration")}
+                className="button3"
+              >
+                Inspirations
+              </a>
+              <a
+                onClick={() => scrollToSection("plantshop")}
+                className="button4"
+              >
+                Plant Shop
+              </a>
+            </nav>
+          )}
         </div>
-        <div className="title">
-          {/* Simple and straightforward title */}
-          <h6>PlantFashion</h6>
+
+        <div className="main">
+          <div className="title">
+            {/* Simple and straightforward title */}
+            <h6>PlantFashion</h6>
+          </div>
+          <div className="text">
+            {/* Large, eye-catching text that draws attention */}
+            <h1>
+              <span>Find the perfect </span>
+              <span>complement</span>
+              <span> to your room</span>
+            </h1>
+          </div>
+          <div className="paragraph">
+            {/* Paragraph below the large text, providing additional information */}
+            <p>We can help you find plants that suit your needs and style.</p>
+            <p>
+              Check out our selection in the store and surround yourself with
+              greenery!
+            </p>
+          </div>
+          {/* Button that takes the user to the store to buy plants */}
+          <button className="btn-now">Buy now</button>
         </div>
-        <div className="text">
-          {/* Large, eye-catching text that draws attention */}
-          <h1>
-            <span>Find the perfect </span>
-            <span>complement</span>
-            <span> to your room</span>
-          </h1>
-        </div>
-        <div className="paragraph">
-          {/* Paragraph below the large text, providing additional information */}
-          <p>We can help you find plants that suit your needs and style.</p>
-          <p>
-            Check out our selection in the store and surround yourself with
-            greenery!
-          </p>
-        </div>
-        {/* Button that takes the user to the store to buy plants */}
-        <button className="btn-now">Buy now</button>
+
         {/* Large image of a plant, prominently displayed */}
         <div className="image">
           <a href="https://ibb.co/vLGWZhM">
